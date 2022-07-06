78d50b8e1d79c2e40029ff90632c95676778cf3c
Guilherme Ribeiro (GR) <61317250+guihrib@users.noreply.github.com>
2022-07-06 20:05:26 +0000

    a

--- src/structures/Manager.js
+++ src/structures/Manager.js
@@ -16,5 +16,7 @@ module.exports = (client) => {
     sendWS: (guildId, payload) => { 
       client.guilds.cache.get(guildId)?.shard.send(payload);
   })
-  
+  .on('nodeConnect', async(node) => {
+console.log(`[NODE] ${node.options.id} Conectado.`)
+  })
 }
