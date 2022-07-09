6f315ed568c29f26dcbd12bfc49236c06c49e8ab
guihrib <gr.miraidevelopment@gmail.com>
2022-07-09 15:57:31 -0300

    new deploy

--- src/utils/Deploy.js
+++ src/utils/Deploy.js
@@ -2,27 +2,31 @@ import { config } from 'dotenv';
 config()
 
 import { REST } from '@discordjs/rest'
-import { Routes } from 'discord-api-types/v9'
+import { Routes } from 'discord-api-types/v10'
 import { readdirSync } from 'fs'
 
 export default (client) => {
 
-  const rest = new REST({ version: '9' }).setToken(client.token);
+const commands = [];
+readdirSync('./src/commands').forEach((pasta) => {
+  readdirSync(`./src/commands/${pasta}`).filter(file => file.endsWith('.js')).forEach(async (command) => {
+    commands.push(command)
+  })
+})
+this.client.application.commands.set(commands)
+
+  const rest = new REST({ version: '10' }).setToken(client.token);
 
   (async () => {
       try {
 
-          console.log('[Slash Commands] Atualização dos comandos iniciada.');
+          console.log('[Slash Commands] Atualização dos comando iniciada.');
 
-          const commands = [];
-          readdirSync('./src/commands').forEach((pasta) => {
-            readdirSync(`./src/commands/${pasta}`).filter(file => file.endsWith('.js')).forEach(async (command) => {
-              commands.push(command)
-            })
+          await rest.put(Routes.applicationCommands(client.user.id), { body: commands }).then(cmd => {
+            cmd.map(() => {})
           })
-          this.client.application.commands.set(commands)
 
-         console.log('[Slash Commands] Atualização dos comandos concluída.');
+         console.log('[Slash Commands] Atualização dos comando concluída.');
 
       } catch (error) {
           console.error(error);
