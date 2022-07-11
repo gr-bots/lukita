b01f13783d0b37506fc462172895d5eac651ee1f
guihrib <gr.miraidevelopment@gmail.com>
2022-07-11 18:19:49 -0300

    new deploy

--- src/utils/Client.js
+++ src/utils/Client.js
@@ -6,7 +6,6 @@ import events from '../handlers/Events.js'
 import commands from '../handlers/Commands.js'
 import modals from '../handlers/Modals.js'
 import deploy from '../utils/Deploy.js'
-import prismaClient from '../utils/PrismaClient.js'
 
 export default class LukitaClient extends Client {
   constructor() {
@@ -48,7 +47,6 @@ export default class LukitaClient extends Client {
     await commands(this)
     await modals(this)
     await database(this)
-    await prismaClient(this)
     
     await super.login(process.env.TOKEN)
     await deploy(this)
