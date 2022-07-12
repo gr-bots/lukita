5def5c9314ddb30258006e153d5d096772d7cd43
guihrib <gr.miraidevelopment@gmail.com>
2022-07-12 15:38:59 -0300

    new deploy

--- src/utils/Deploy.js
+++ src/utils/Deploy.js
@@ -10,7 +10,7 @@ export default async(client) => {
         const files = await fs.readdirSync(`./src/commands/${pasta}`).filter(file => file.endsWith('.js'))
         files.forEach(async (commands) => {
           const { default: listCommands } = await import(`../../src/commands/${pasta}/${commands}`)
-          console.log(listCommands.map(cmd => cmd.data)) // await client.application.commands.set(listCommands.map(cmd => cmd.data))
+          console.log(typeof(listCommands)) // await client.application.commands.set(listCommands.map(cmd => cmd.data))
         })
       })
       console.log('[ / Slash Commands ] Atualização dos comandos concluída.');
