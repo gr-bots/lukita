bc2ac2750426d9e6f1e17984b1083c1d5b473672
guihrib <gr.miraidevelopment@gmail.com>
2022-07-13 13:50:27 -0300

    new deploy

--- src/utils/Client.js
+++ src/utils/Client.js
@@ -66,8 +66,8 @@ export default class LukitaClient extends Client {
   
     const arrayOfSlashCommands = [];
     slashCommands.map(async (value) => {
-      console.log(value)
       const file = await import(value);
+      console.log(file)
   
       if(!file?.name || !file.description ||!file.options) return;
   
