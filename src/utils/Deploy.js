024b42936e653822a891cb6f39dd50ddd1ea7b43
guihrib <gr.miraidevelopment@gmail.com>
2022-07-14 10:32:59 -0300

    Deploy finalmente terminado kk

--- src/utils/Deploy.js
+++ src/utils/Deploy.js
@@ -2,23 +2,23 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 
 export default async (client) => {
-let arrayOfCommands = Array()
-let map = Array.from(client.commands)
-for (let command of Object(map)) {
-  command = command[1].data
-  arrayOfCommands.push(command)
-}
+  let arrayOfCommands = Array()
+  let map = Array.from(client.commands)
+  for (let command of Object(map)) {
+    command = command[1].data
+    arrayOfCommands.push(command)
+  }
 
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
+  const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
 
-try {
-  console.log('[ / Slash Commands ] Atualização de comandos iniciada...');
-  await rest.put(
-    Routes.applicationCommands(client.user.id),
-    { body: arrayOfCommands },
-  );
-  console.log('[ / Slash Commands ] Atualização de comandos concluída!');
-} catch (error) {
-  console.error(error);
-}
+  try {
+    console.log('[ / Slash Commands ] Criação e atualização de comandos iniciada...');
+    await rest.put(
+      Routes.applicationCommands(client.user.id),
+      { body: arrayOfCommands },
+    );
+    console.log('[ / Slash Commands ] Criação e atualização de comandos concluída!');
+  } catch (error) {
+    console.error(error);
+  }
 }
