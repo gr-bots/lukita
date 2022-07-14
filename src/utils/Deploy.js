4af22d25abc46aab1d2a6eae24abb64b3a5127f7
guihrib <gr.miraidevelopment@gmail.com>
2022-07-14 00:30:53 -0300

    new deploy

--- src/utils/Deploy.js
+++ src/utils/Deploy.js
@@ -12,23 +12,11 @@ for (let command of Object(map)) {
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
 try {
   console.log('Started refreshing application (/) commands.');
-
-  await rest.put(
-    Routes.applicationGuildCommands(client.user.id, "724823792794337301"),
-    { body: arrayOfCommands },
-  );
-
-  console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-  console.error(error);
-}
-try {
-  //console.log('Started refreshing application (/) commands.');
   await rest.put(
     Routes.applicationCommands(client.user.id),
     { body: arrayOfCommands },
   );
-  //console.log('Successfully reloaded application (/) commands.');
+  console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
   console.error(error);
 }
