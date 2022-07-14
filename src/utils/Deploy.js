048dc6b1724f2f84b737aca01edfc1b2a1dbff5e
guihrib <gr.miraidevelopment@gmail.com>
2022-07-14 18:22:38 -0300

    new deploy

--- src/utils/Deploy.js
+++ src/utils/Deploy.js
@@ -5,16 +5,18 @@ export default async (client) => {
   let arrayOfCommands = Array()
   let map = Array.from(client.commands)
   for (let command of Object(map)) {
-    console.log(command)
     command = command[1].data
     if (command) arrayOfCommands.push(command)
   }
 
-  console.log(arrayOfCommands)
   const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
 
   try {
     console.log('[ / Slash Commands ] Criação e atualização de comandos iniciada...');
+
+    console.log(`[ / ListCommands] Comandos encontrados: \n| ${client.commands.map(a => a.name).toString().replaceAll(',', ', ')} |`);
+    console.log(`[ / ListCommands] Quantidade de comandos encontrados: ${arrayOfCommands.length}`);
+
     await rest.put(
       Routes.applicationCommands(client.user.id),
       { body: arrayOfCommands },
