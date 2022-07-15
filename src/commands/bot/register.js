0365b890e761f6194d1771b0774b0258cbe0f7e6
guihrib <gr.miraidevelopment@gmail.com>
2022-07-15 11:51:31 -0300

    new deploy

---
+++ src/commands/bot/register.js
@@ -0,0 +1,22 @@
+import { MessageEmbed } from 'discord.js';
+
+export default {
+    name: "register",
+    data: {
+        name: "register",
+        description: "Comando temporário",
+        type: 1,
+        options: []
+    },
+    category: 'Bot',
+    view: false,
+    devsOnly: true,
+    run: async (client, interaction) => {
+        let register = new MessageEmbed()
+            .setAuthor({ iconURL: `${client.user.displayAvatarURL({ dynamic: true, size: 4096, format: 'png' })}`, name: `${client.user.tag}` })
+            .setDescription('a')
+            .setColor(client.pallete.noBG)
+            .setFooter({ text: `${interaction.guild.name}}`, iconURL: `${interaction.guild.iconURL({ dynamic: true })}` })
+        interaction.channel.send({embeds: [register]})
+    }
+}
