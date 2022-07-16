d81a0631d404ac77ac08dbb4c3aef0510b4f4181
Guilherme Ribeiro (GR) <gr.miraidevelopment@gmail.com>
2022-07-16 17:58:12 -0300

    Update staff.js

---
+++ src/modals/forms/staff.js
@@ -0,0 +1,8 @@
+import { codeBlock, inlineCode } from '@discordjs/builders'
+import { MessageEmbed, MessageAttachment, MessageActionRow, MessageButton } from 'discord.js'
+
+export default {
+    name: 'eval_modal',
+    run: async (client, interaction) => {
+    }
+}
