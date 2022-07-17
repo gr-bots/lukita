f3b2a189bfe41ab6e7504ce623ff42926cca21eb
Guilherme Ribeiro (GR) <gr.miraidevelopment@gmail.com>
2022-07-16 21:21:31 -0300

    Create DCLcommand.js

---
+++ src/structures/DCLcommand.js
@@ -0,0 +1,10 @@
+class DCLcommand {
+  constructor(client, options) {
+     this.client = client
+     this.name = options.name
+     this.description = options.description
+    this.a = options.a
+    this.options = options.options
+   }
+}
+export default DCLcommand
