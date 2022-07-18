097fa4e0883f5e3089cc7b4ee1a8f944509fcb90
guihrib <gr.miraidevelopment@gmail.com>
2022-07-18 13:03:23 -0300

    new deploy

--- src/modals/config/form.js
+++ src/modals/config/form.js
@@ -5,6 +5,8 @@ export default {
     name: 'form_modal',
     run: async (interaction) => {
         let resposta1 = interaction.getTextInputValue('resposta1');
+        
+        if (!resposta1) return interaction.reply({ content: `> ${client.emotes.alert}・<@${interaction.member.id}>, Você precisa inserir uma reposta para ser enviado.` })
         interaction.reply({content: `${resposta1}`})
     }
 }
