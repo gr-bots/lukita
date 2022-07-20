1d270b7fd22dc006f7437591fb632ffb63d49ae5
zMica <andrademicae@gmail.com>
2022-07-20 13:30:32 -0300

    ok!

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ import { config } from 'dotenv'; config()
 import LukitaClient from './src/structures/Client.js';
 const client = new LukitaClient();
 client.init()
-
-import Modals from 'discord-modals'
-Modals(client)
