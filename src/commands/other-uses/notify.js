8fe30c0d90b4605e9f2237228b0b6fdbfb21068d
guihrib <gr.miraidevelopment@gmail.com>
2022-07-21 22:54:12 -0300

    new deploy

--- src/commands/other-uses/notify.js
+++ src/commands/other-uses/notify.js
@@ -12,18 +12,12 @@ export default {
     view: false,
     devsOnly: true,
     run: async (client, interaction) => {
-        interaction.followUp('🔔 Notify');
         let register = new EmbedBuilder()
             .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 4096 }))
             .setAuthor({ iconURL: `${client.user.displayAvatarURL({ size: 4096, format: 'png' })}`, name: `${client.user.tag}` })
             .setDescription(`Para não marcarmos @everyone toda hora, criamos esses cargos específicos. Reaja nos emojis correspondentes para obter os cargos.\n\n📢 <@&995850502153711787>\n🤝 <@&995850735386370048>\n🍃 <@&995850976860831784>\n🎉 <@&999851022228918366>\n⚰️ <@&999850970152448174>`)
             .setColor(client.pallete.noBG)
             .setFooter({ text: `${interaction.guild.name}`, iconURL: `${interaction.guild.iconURL({ dynamic: true })}` })
-        interaction.channel.send({embeds: [register]})
-        interaction.react('📢')
-        interaction.react('🤝')
-        interaction.react('🍃')
-        interaction.react('🎉')
-        interaction.react('⚰️')
+        interaction.channel.send({embeds: [register], fetchReply: true});
     }
 }
