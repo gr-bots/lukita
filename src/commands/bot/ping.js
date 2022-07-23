5c3ff343ec77bdaa3749127747f7a4045bbf8ab5
guihrib <gr.miraidevelopment@gmail.com>
2022-07-23 20:31:00 -0300

    new deploy

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -13,9 +13,9 @@ export default {
     devsOnly: false,
     run: async (client, interaction) => {
 
-        async function ping() {
+        async function pingMongo() {
             const pingStart = process.hrtime();
-            await this.db.guild.findOne({ _id: interaction.guild.id });
+            await client.db.guild.findOne({ _id: interaction.guild.id });
             const pingStop = process.hrtime(pingStart);
             const pingDb = Math.round(((pingStop[0] * 1e9) + pingStop[1]) / 1e6);
             return pingDb;
@@ -31,7 +31,7 @@ export default {
         
         let embed = new EmbedBuilder()
             .setAuthor({ iconURL: `${authorAvatarURL}`, name: `${interaction.member.user.tag}` })
-            .setDescription(`> ${client.emotes.signal} Gateaway \`${Math.round(client.ws.ping)}ms\`\n> ${client.emotes.lighting} API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n${client.emotes.mongodb} MongoDB \`${await ping()}\`\n> ${client.emotes.firebase} Firebase \`${ping}ms\``)
+            .setDescription(`> ${client.emotes.signal} Gateaway \`${Math.round(client.ws.ping)}ms\`\n> ${client.emotes.lighting} API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n${client.emotes.mongodb} MongoDB \`${await pingMongo()}\`\n> ${client.emotes.firebase} Firebase \`${ping}ms\``)
             .setColor(client.pallete.noBG)
             .setFooter({ text: 'Solicitado' })
             .setTimestamp()
