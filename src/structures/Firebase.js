cbce1e28b35ccd354c5825422b6f93110f3f26fc
guihrib <gr.miraidevelopment@gmail.com>
2022-07-24 18:33:59 -0300

    new deploy

--- src/structures/Firebase.js
+++ src/structures/Firebase.js
@@ -6,13 +6,11 @@ export default (client) => {
   const app = initializeApp(fbConfig)
   const db = getDatabase(app);
   class Firebase {
-    constructor(dir) {
-      this.ref = ref(db, dir);
-      this.onValue = onValue(this.ref);
-    }
-    async set(path, value) {
-      return set(this.ref, path, value);
+    constructor (args) {
+      this.db = db
+      this.ref = ref(args)
+      this.set = set(args)
     }
   }
-  client.fb = new Firebase();
+  client.fb = new Firebase()
 }
