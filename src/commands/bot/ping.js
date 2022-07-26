a6c85e4ecc960347b1f29d654792975a47f392a0
guihrib <gr.miraidevelopment@gmail.com>
2022-07-25 22:20:32 -0300

    new deploy

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -20,15 +20,6 @@ export default {
             const pingDb = Math.round(((pingStop[0] * 1e9) + pingStop[1]) / 1e6);
             return pingDb;
         }
-        
-        async function pingFb() {
-            const pingStartFB = process.hrtime();
-            await client.fb.ref('ping').once('value');
-            const pingStopFB = process.hrtime(pingStartFB);
-            const pingFB = Math.round(((pingStopFB[0] * 1e9) + pingStopFB[1]) / 1e6);
-            return pingFB;
-        }
-
         const authorAvatarURL = interaction.member.displayAvatarURL({ dynamic: true })
         
         let bahzin = await interaction.reply({ 
@@ -38,7 +29,7 @@ export default {
         
         let embed = new EmbedBuilder()
             .setAuthor({ iconURL: `${authorAvatarURL}`, name: `${interaction.member.user.tag}` })
-            .setDescription(`> ${client.emotes.signal} Gateaway \`${Math.round(client.ws.ping)}ms\`\n> ${client.emotes.lighting} API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n> ${client.emotes.mongodb} MongoDB \`${await pingMongo()}ms\`\n> ${client.emotes.firebase} Firebase \`${await pingFb()}ms\``)
+            .setDescription(`> ${client.emotes.signal} Gateaway \`${Math.round(client.ws.ping)}ms\`\n> ${client.emotes.lighting} API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n> ${client.emotes.mongodb} MongoDB \`${await pingMongo()}ms\``)
             .setColor(client.pallete.noBG)
             .setFooter({ text: 'Solicitado' })
             .setTimestamp()
--- src/structures/Firebase.js
+++ src/structures/Firebase.js
@@ -1,8 +1,7 @@
 import { fbConfig } from '../utils/Config.js';
-import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, set, onValue } from 'firebase/database'
+import { Firebase as FirebaseUtil } from 'firebase-util.js';
+const db = new FirebaseUtil(fbConfig);
 
 export default (client) => {
-  const app = initializeApp(fbConfig)
-  client.fb = getDatabase(app);
+  client.fb = db
 }
