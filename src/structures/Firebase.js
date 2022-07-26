ada3ade59c69e0158e33d3026d264be6e3d30b54
guihrib <gr.miraidevelopment@gmail.com>
2022-07-25 22:58:34 -0300

    new deploy

--- src/structures/Firebase.js
+++ src/structures/Firebase.js
@@ -1,7 +1,7 @@
 import { fbConfig } from '../utils/Config.js';
 import pkg from 'firebase-util.js';
 const { FirebaseUtil } = pkg;
-const db = new FirebaseUtil(fbConfig);
+const db = FirebaseUtil(fbConfig);
 
 export default (client) => {
   client.fb = db
