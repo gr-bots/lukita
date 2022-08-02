15645c4b0e572207b132d1232dec4123ecdda71a
guihrib <gr.miraidevelopment@gmail.com>
2022-08-01 23:49:17 -0300

    new deploy

--- src/events/ready.js
+++ src/events/ready.js
@@ -9,6 +9,6 @@ export default class extends Event {
   async run() {
     this.client.manager?.init(client.user.id);
 
-    this.client.user.setPresence({ activities: [{ name: '💙› Use {/help}' }], status: 'idle' });
+    this.client.user.setPresence({ activities: [{ name: '📘› Use {/help}' }]});
   }
 }
