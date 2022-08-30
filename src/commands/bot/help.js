cedfb27e5e818273a14f77fd00ff8723a0cf5ec0
GR <gr.miraidevelopment@gmail.com>
2022-08-30 00:54:07 -0300

    new version

--- src/commands/bot/help.js
+++ src/commands/bot/help.js
@@ -12,12 +12,11 @@ export default class PingCommand extends Command {
 
   async execute({ interaction }) {
     let embedHelp = new EmbedBuilder()
-      .setThumbnail(`${this.client.user.displayAvatarURL({ format: 'png', size: 4096 })}`)
       .setAuthor({ name: `${this.client.user.username}・Help`, iconURL: `${interaction.guild.iconURL({dynamic: true, size: 4096})}` })
       .setTitle('Lista de comandos')
       .setDescription(`:wink: **${interaction.member.user.tag}** seja bem vindo a minha central de ajuda e help dos meus comandos. \n\n${this.client.emj.categBot} › Informações (Bot) \`[ 3 ]\` \n</botinfo:997023687356186685> - </help:998743092612055060> - </ping:997023687356186686>`)
       .setColor(this.client.clr.blueBaby)
       .setFooter({ text: `Sou um jovem disposto a deixar seu servidor melhor e mais divertido sendo multifuncional. ` })
-    interaction.reply({ embeds: [embedHelp] })
+    interaction.reply({ embeds: [embedHelp], iconURL: `${this.client.user.displayAvatarURL({ format: 'png', size: 4096 })}` })
   }
 }
