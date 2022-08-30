4d48d0f8f2240a3589878bdae13b72705a61aaea
its.gr6603@gmail.com <its.gr6603@gmail.com>
2022-08-30 11:03:49 -0300

    testing eval

--- src/commands/developer/eval.js
+++ src/commands/developer/eval.js
@@ -1,4 +1,5 @@
 import { ApplicationCommandType, EmbedBuilder } from 'discord.js';
+import { inspect } from 'util';
 import { Command } from '../../structures/Command.js';
 
 export default class EvalCommand extends Command {
@@ -7,11 +8,25 @@ export default class EvalCommand extends Command {
       name: 'eval',
       description: '「✨ Developer」・  Execução de códigos do meu desenvolvedor',    
       type: ApplicationCommandType.ChatInput,
+      options: [{ 
+        name: 'code',
+        description: 'Digite o código',
+        type: ApplicationCommandOptionType.String, 
+        required: true
+      }],
     });
     this.devOnly = true
   }
 
   async execute({ interaction }) {
-    
+    try {
+      const code = await eval(interaction.args.join(' '));
+      const codeLeave = typeof code !== 'string' ? inspect(code, { depth: 0 }).replaceAll(this.client.token, 'hidden') : code.replaceAll(this.client.token, 'hidden');
+      return interaction.reply({ content: `\`\`\`js\n${codeLeave.slice(0, 1900)}\`\`\``, ephemeral: true });
+    } catch (err) {
+      if (err instanceof Error) {
+        return interaction.reply({ content: `\`\`\`js\n${err.stack}\`\`\``, ephemeral: true });
+      }
+    }
   }
 }
