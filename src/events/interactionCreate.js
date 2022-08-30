940a1faa13f9ee5fbd42db8c60862e5f12e1323b
its.gr6603@gmail.com <its.gr6603@gmail.com>
2022-08-30 10:46:29 -0300

    new version

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -14,12 +14,12 @@ export default class InteractionCreate extends Event {
     command?.execute({ interaction });
     try {
         if (command.devsOnly && !client.dev.includes(interaction.member.id)) {
-                return interaction.reply({ content: `⚠️・<@${interaction.member.id}>, Você não é um dos meus desenvolvedores.`, ephemeral: true })
+                return interaction.reply({ content: `⚠️・<@${interaction.member.id}>, Você não é um dos meus desenvolvedores.`, fetchReply: true, ephemeral: true })
                 }
                 await command.run(this.client, interaction);
         } catch (error) {
             console.log(error)
-            await interaction.reply({ content: `> ⚠️・<@${interaction.member.id}>, Ocorreu um erro ao executar o comando! Já avisei os meus desenvolvedores.`, fetchReply: true, ephemeral: true });
+            await interaction.reply({ content: `> ⚠️・<@${interaction.member.id}>, Ocorreu um erro ao executar o comando! Já avisei os meus desenvolvedores.`, ephemeral: true });
         }
   }
 }
