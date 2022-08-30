8361b8c20863d6a2e5ca2f05b0ce12a9668e9879
its.gr6603@gmail.com <its.gr6603@gmail.com>
2022-08-30 13:15:20 -0300

    fix bugs in interactionCreate event

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -13,10 +13,9 @@ export default class InteractionCreate extends Event {
         const command = client.commands.getCommand(interaction.commandName);
 
         try {
-            if (command.devOnly && !client.dev.includes(interaction.user.id)) {
-                return interaction.reply({ content: `⚠️・<@${interaction.user.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true })
-            }
-            await command?.execute({ client, interaction })
+            if (command.devOnly === true && !client.dev.some((id) => id === interaction.user.id)) return interaction.reply({ content: `⚠️・<@${interaction.user.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true });
+    
+            await command?.execute({ client, interaction });
         } catch (error) {
             await interaction.reply({ content: `> ⚠️・<@${interaction.user.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true });
         }
