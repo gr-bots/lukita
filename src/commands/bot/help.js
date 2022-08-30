a63195600844de02afc57ca6231f75ecd0d1a47d
guihrib <gr.miraidevelopment@gmail.com>
2022-08-30 16:16:27 -0300

    new deploy

--- src/commands/bot/help.js
+++ src/commands/bot/help.js
@@ -2,6 +2,7 @@ import { ApplicationCommandType, EmbedBuilder } from 'discord.js';
 import { Command } from '../../structures/Command.js';
 import { emjs } from '../../utils/Config.js';
 import { Pallete } from '../../utils/Functions.js'
+const clr = Pallete
 
 export default class HelpCommand extends Command {
   constructor(client) {
@@ -18,7 +19,7 @@ export default class HelpCommand extends Command {
       .setAuthor({ name: `${this.client.user.username}・Help`, iconURL: `${interaction.guild.iconURL({dynamic: true, size: 4096})}` })
       .setTitle('Lista de comandos')
       .setDescription(`:wink: **${interaction.member.user.tag}** seja bem vindo a minha central de ajuda e help dos meus comandos. \n\n${emjs.categBot} › Informações (Bot) \`[ 3 ]\` \n</botinfo:997023687356186685> - </help:998743092612055060> - </ping:997023687356186686>`)
-      .setColor(Pallete.blueBaby)
+      .setColor(`${clr.blueBaby}`)
       .setFooter({ text: `Sou um jovem disposto a deixar seu servidor melhor e mais divertido sendo multifuncional. ` })
     interaction.reply({ embeds: [embedHelp], iconURL: `${this.client.user.displayAvatarURL({ format: 'png', size: 4096 })}` })
   }
