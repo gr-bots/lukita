10d72c759c0a47eb5d96280ffb508bf23f0ea7cd
its.gr6603@gmail.com <its.gr6603@gmail.com>
2022-08-30 12:51:32 -0300

    check for eval with just developer id

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,22 +1,22 @@
 import { Event } from '../structures/Event.js';
 
 export default class InteractionCreate extends Event {
-  constructor() {
-    super();
-    this.eventName = 'interactionCreate';
-  }
+    constructor() {
+        super();
+        this.eventName = 'interactionCreate';
+    }
 
-  async execute(client, interaction) {
-    if (!interaction.isChatInputCommand()) return;
-    if (!interaction.guild) return;
+    async execute(client, interaction) {
+        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.guild) return;
 
-    const command = client.commands.getCommand(interaction.commandName);
+        const command = client.commands.getCommand(interaction.commandName);
 
-    
-    try {
-        if (command.devOnly === true && !client.dev.some((id) => id === interaction.user.id)) {
-        return interaction.reply({ content: `⚠️・<@${interaction.member.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true })
-        } command?.execute({ interaction });
+        try {
+            if (command.devOnly === true && !client.dev.some((id) => id === interaction.user.id)) {
+                return interaction.reply({ content: `⚠️・<@${interaction.member.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true })
+            }
+            await command?.execute({ interaction });
         } catch (error) {
             await interaction.reply({ content: `> ⚠️・<@${interaction.member.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true });
         }
