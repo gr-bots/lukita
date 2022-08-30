6b8f83b841e5c8265c826c4f1ca5fac376fb72c5
its.gr6603@gmail.com <its.gr6603@gmail.com>
2022-08-30 12:46:29 -0300

    new eval

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -8,14 +8,15 @@ export default class InteractionCreate extends Event {
 
   async execute(client, interaction) {
     if (!interaction.isChatInputCommand()) return;
+    if (!interaction.guild) return;
 
     const command = client.commands.getCommand(interaction.commandName);
 
-    command?.execute({ interaction });
+    
     try {
         if (command.devOnly === true && !client.dev.some((id) => id === interaction.user.id)) {
         return interaction.reply({ content: `⚠️・<@${interaction.member.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true })
-                }
+        } command?.execute({ interaction });
         } catch (error) {
             await interaction.reply({ content: `> ⚠️・<@${interaction.member.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true });
         }
