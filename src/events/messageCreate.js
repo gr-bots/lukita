8f954bd9dd41eab0d929f576c620762b53187a96
its.gr6603@gmail.com <its.gr6603@gmail.com>
2022-09-02 09:42:35 -0300

    updating event of messageCreate

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -14,7 +14,7 @@ export default class MessageCreate extends Event {
     if(message.content == `<@${client.user.id}>`) {
       const rowMention = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel('Me adicione em seu servidor').setStyle(ButtonStyle.Link).setURL('https://discord.com/oauth2/authorize?client_id=917962601923760139&scope=bot+identify+guilds+email+applications.commands&permissions=2080374975'));
 
-      message.reply({ content: `> :man_raising_hand::skin-tone-1:・Olá **${message.author.username}**, sou um jovem disposto a deixar sua moderação muito mais fácil. Sou um bot feito em SlashCommands, sendo assim, use </help:998743092612055060> para ver meus comandos.`, components: [rowMention] })
+      message.reply({ content: `> :man_raising_hand::skin-tone-1:・Olá **${message.author.username}**, sou um jovem disposto a deixar sua moderação muito mais fácil.\nSou um bot feito em SlashCommands, sendo assim, use </help:998743092612055060> para ver meus comandos.`, components: [rowMention] })
     }
   }
 }
