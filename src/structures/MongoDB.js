dc33b7acf029c86578d3b6c02c3517b5474cb9e8
guihrib <gr.miraidevelopment@gmail.com>
2022-09-06 14:03:51 -0300

    mongodb ping function

--- src/structures/MongoDB.js
+++ src/structures/MongoDB.js
@@ -6,6 +6,14 @@ export default (client) => {
       this.user = User,
       this.guild = Guild;
     }
+    
+    async ping() {
+      const pingStart = process.hrtime();
+      await this.guild?.findOne({ _id: client.user.id });
+      const pingStop = process.hrtime(pingStart);
+  
+      return Math.round(((pingStop[0] * 1e9) + pingStop[1]) / 1e6);
+    }
   }
   client.db = new Database();
 }
