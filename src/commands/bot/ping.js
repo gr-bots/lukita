5062564b10b77a2f806592831e0673cdee8bac89
guihrib <gr.miraidevelopment@gmail.com>
2022-09-06 15:52:38 -0300

    ping ready

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -16,7 +16,7 @@ export default class PingCommand extends Command {
     const bahzin = await interaction.reply({ content: `${emjs.loading}`, fetchReply: true });
     const wsPing = Math.round(this.client.ws.ping);
 
-    let pingContent = `> :satellite: Gateaway \`${wsPing}ms\`\n> :zap: API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n> ${emjs.mongodb} Database \`${this.client.db.ping()}ms\``;
+    let pingContent = `> :satellite: Gateaway \`${wsPing}ms\`\n> :zap: API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n> ${emjs.mongodb} Database \`${await this.client.db.ping()}ms\``;
 
     setTimeout(() => { interaction.editReply({ content: `${pingContent}` }); }, 1000);
   }
