518be5e5c96482e9906849320535300111ceda5a
guihrib <gr.miraidevelopment@gmail.com>
2022-09-06 14:24:56 -0300

    new deploy

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -2,14 +2,6 @@ import { ApplicationCommandType } from 'discord.js';
 import { Command } from '../../structures/Command.js';
 import { emjs } from '../../utils/Emojis.js';
 
-async function pingMongo(client, interaction) {
-  const pingStart = process.hrtime();
-  await client.db.guild.findOne({ _id: interaction.guild.id });
-  const pingStop = process.hrtime(pingStart);
-  const pingDb = Math.round(((pingStop[0] * 1e9) + pingStop[1]) / 1e6);
-  return pingDb;
-}
-
 export default class PingCommand extends Command {
   constructor(client) {
     super(client, {
@@ -24,7 +16,7 @@ export default class PingCommand extends Command {
     const bahzin = await interaction.reply({ content: `${emjs.loading}`, fetchReply: true });
     const wsPing = Math.round(this.client.ws.ping);
 
-    let pingContent = `> :satellite: Gateaway \`${wsPing}ms\`\n> :zap: API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n> ${emjs.mongodb} Database \`${await pingMongo(this.client, interaction)}ms\``;
+    let pingContent = `> :satellite: Gateaway \`${wsPing}ms\`\n> :zap: API \`${bahzin.createdTimestamp - interaction.createdTimestamp}ms\`\n> ${emjs.mongodb} Database \`${this.client.db.ping()}ms\``;
 
     setTimeout(() => { interaction.editReply({ content: `${pingContent}` }); }, 1000);
   }
