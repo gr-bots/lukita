a0f689f05626cd5517ac103467925ad9b480c951
guihrib <gr.miraidevelopment@gmail.com>
2022-09-08 19:30:53 -0300

    small changes

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -17,8 +17,8 @@ export default class InteractionCreate extends Event {
     
             await command?.execute({ client, interaction });
         } catch (error) {
-            await interaction.deferReply({ content: `> ⚠️・<@${interaction.user.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true, fetchReply: true });
             client.logger.error(error, { tags: ['Error'] })
+            await interaction.reply({ content: `> ⚠️・<@${interaction.user.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true });
         }
     }
 }
