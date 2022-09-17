24113a43a1bfb249e116dfa5858f67a09cee5a26
guihrib <gr.miraidevelopment@gmail.com>
2022-09-16 22:21:59 -0300

    event update

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -10,6 +10,6 @@ export default class GuildCreate extends Event {
     const Guild = await client.db.guild.findOne({ _id: guild.id });
     if (!Guild) await client.db.guild.create({ _id: guild.id });
 
-    client.channels.cache.get('998201539929845760').send({ content: `Nova guild: ${guild.name} - ${guild.id}` });
+    client.channels.cache.get('1020491024021336214').send({ content: `Nova guild: ${guild.name} - ${guild.id}` });
   }
 }
