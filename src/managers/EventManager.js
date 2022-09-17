e453d61dbf8e76d0def7c7c3c280421c1ca64264
guihrib <gr.miraidevelopment@gmail.com>
2022-09-17 16:24:22 -0300

    small changes

--- src/managers/EventManager.js
+++ src/managers/EventManager.js
@@ -8,7 +8,7 @@ export class EventManager {
   async loadEvents() {
     const events = (await readdir(`${process.cwd()}/src/events/`)).filter(file => file.endsWith('.js'));
     for await (const event of events) {
-      const { default: EventClass } = await import(`${process.cwd()}/src/events/${event}`);
+      const { default: EventClass } = await import(`${process.cwd()}/events/${event}`);
       const evt = new EventClass();
       this.client.on(evt.eventName, (...args) => evt.execute(this.client, ...args));
     }
