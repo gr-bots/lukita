c755c45a0233e08452fb01f0e5a3732fed69a7ea
guihrib <gr.miraidevelopment@gmail.com>
2022-09-17 17:24:02 -0300

    updated shell

--- src/commands/developer/shell.js
+++ src/commands/developer/shell.js
@@ -21,14 +21,13 @@ export default class ShellCommand extends Command {
   }
 
   async execute({ interaction }) {
-    const REGEX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
     const comando = interaction.options.getString('comando');
     
     exec(comando , (err, res) => {
       if (err) {
-        return interaction.reply({ content: `**${emjs.noCheck}・Error:**\n\`\`\`${err}\`\`\``, ephemeral: true });
+        return interaction.reply({ content: `**${emjs.noCheck}・Error:**\n\`\`\`sh\n${err}\`\`\``, ephemeral: true });
       } else {
-        return interaction.reply({ content: `**${emjs.yesCheck}・Output:**\n\`\`\`\n${res.replace(REGEX, '').slice(0, 1900)}\`\`\``, ephemeral: true });
+        return interaction.reply({ content: `**${emjs.yesCheck}・Output:**\n\`\`\`sh\n${res.replace(REGEX, '').slice(0, 1900)}\`\`\``, ephemeral: true });
       }
     });
   }
