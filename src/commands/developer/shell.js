e40d4b81f8b32220133fa406a9fa07369ca27a36
guihrib <gr.miraidevelopment@gmail.com>
2022-09-18 20:35:51 -0300

    updated shell

--- src/commands/developer/shell.js
+++ src/commands/developer/shell.js
@@ -2,6 +2,8 @@ import { ApplicationCommandType, ApplicationCommandOptionType } from 'discord.js
 import { exec } from 'child_process';
 import { Command } from '../../structures/Command.js';
 import { emjs } from '../../utils/Emojis.js';
+const RGX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
+
 export default class ShellCommand extends Command {
   constructor(client) {
     super(client, {
@@ -22,13 +24,10 @@ export default class ShellCommand extends Command {
     const comando = interaction.options.getString('comando');
     
     exec(comando, (res) => {
-      const RGX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
-      try {
-        return interaction.reply({ content: `**${emjs.yesCheck}・Output:**\n\`\`\`bash\n${res.replace(RGX, '').slice(0, 1900)}\`\`\``, ephemeral: true });
-      } catch (err) {
-        if (err instanceof Error) {
-          return interaction.reply({ content: `**${emjs.noCheck}・Error:**\n\`\`\`cmd\n${err.stack}\`\`\``, ephemeral: true, fetchReply: true });
-        }
+      if (err) {
+        return interaction.reply({ content: `**${emjs.noCheck}・Error:**\n\`\`\`cmd\n${err}\`\`\``, ephemeral: true, fetchReply: true });
+      } else {
+        return interaction.reply({ content: `**${emjs.yesCheck}・Output:**\n\`\`\`sh\n${res.replace(RGX, '').slice(0, 1900)}\`\`\``, ephemeral: true, fetchReply: true });
       }
     });
   }
