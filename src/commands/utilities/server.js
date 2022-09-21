b268c7967e1c96fc0278b6ae79ad1b73c899490a
guihrib <gr.miraidevelopment@gmail.com>
2022-09-21 14:42:30 -0300

    server in test

--- src/commands/utilities/server.js
+++ src/commands/utilities/server.js
@@ -35,8 +35,8 @@ export default class ServerinfoCommand extends Command {
     const { client, guild, member, channel } = interaction
     const tools = new Tools(client, interaction)
 
-    if (await interaction.options[0].getSubcommand() === 'info') {
-      let server = await interaction.options[0].getString('id')
+    if (interaction.options[0].getSubcommand() === 'info') {
+      let server = await interaction.options[0].getSubcommand('info').getString('id')
       
       try {
         if (server === null) {
@@ -61,8 +61,8 @@ export default class ServerinfoCommand extends Command {
         console.log(`・Erro no Serverinfo: ${error.stack}`)
         return interaction.reply({ content: `> ⚠️・<@${interaction.user.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true, fetchReply: true });
       }
-    } if (await interaction.options[1].getSubcommand() === 'icon') {
-      let server = await interaction.options[1].getString('id')
+    } if (interaction.options[1].getSubcommand() === 'icon') {
+      let server = await interaction.options[1].getSubcommand('icon').getString('id')
       
       try {
         if (server === null) {
