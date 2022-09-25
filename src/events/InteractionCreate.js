cd339a9182c8a94c7fc7a84683974ac619725362
guihrib <gr.miraidevelopment@gmail.com>
2022-09-25 20:10:24 -0300

    fix bugs in create user

--- src/events/InteractionCreate.js
+++ src/events/InteractionCreate.js
@@ -1,5 +1,4 @@
 import { Event } from '../structures/Event.js';
-
 export default class InteractionCreate extends Event {
     constructor() {
         super();
@@ -12,9 +11,12 @@ export default class InteractionCreate extends Event {
 
         const command = client.commands.getCommand(interaction.commandName);
 
+        const User = await client.db.user.findOne({ _id: interaction.user.id });
+        if (!User) await client.db.user.create({ _id: interaction.user.id });
+
         try {
             if (command.options.devOnly == true && !client.dev.some((id) => id === interaction.user.id)) return interaction.reply({ content: `⚠️・<@${interaction.user.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true });
-            
+
             await command?.execute({ client, interaction });
         } catch (error) {
             client.logger.error(error, { tags: ['Error'] })
