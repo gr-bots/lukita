4432a1c244f4fc4c3fafcc8460da867418f6cf75
guihrib <gr.miraidevelopment@gmail.com>
2022-09-25 15:48:36 -0300

    UserInfos in db updated

--- src/utils/UserInfos.js
+++ src/utils/UserInfos.js
@@ -1,4 +1,4 @@
-export default async (client) => {
-    const user = await client.db.user.findOne({ _id: this.user.id });
-    if (!user) await client.db.user.create({ _id: this.user.id });
+export default async (client, user) => {
+    const user = await client.db.user.findOne({ _id: user.id });
+    if (!user) await client.db.user.create({ _id: user.id });
 }
