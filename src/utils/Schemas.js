40d576f92cf539b82c79591b7a5adce5f309c983
guihrib <gr.miraidevelopment@gmail.com>
2022-09-25 15:14:08 -0300

    user

--- src/utils/Schemas.js
+++ src/utils/Schemas.js
@@ -11,9 +11,11 @@ const Guild = model('Guild', new Schema({
     welcome: { type: Array, default: null },
   },
 }));
-
 const User = model('User', new Schema({
   _id: { type: String, default: null },
 }));
 
-export { Guild, User, connect };
+const user = await client.db.user.findOne({ _id: user.id });
+if (!user) await client.db.user.create({ _id: user.id });
+
+export default (client) = { Guild, User, connect };
