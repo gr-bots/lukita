211b81e4128b5ffdecfebd87b1ae92b188c697b0
guihrib <gr.miraidevelopment@gmail.com>
2022-09-25 22:08:04 -0300

    new update for User's Schema

--- src/utils/Models.js
+++ src/utils/Models.js
@@ -14,7 +14,16 @@ const Guild = model('Guild', new Schema({
 
 const User = model('User', new Schema({
   _id: { type: String, default: null },
-  bl: { type: Boolean, default: false },
+  bl: { type: Boolean, default: false , motivo: { type: String, default: 'Motivo não apresentado' }  },
+  perfil: {
+    xp: { type: Number, default: 0 },
+    level: { type: Number, default: 0 },
+    waffles: { type: Number, default: 0 },
+    caixinhas: { type: Number, default: 0 },
+    sobremim: { type: String, default: 'Não definido' },
+    reps: { type: Number, default: 0 },
+    casamento: { casado: { type: Boolean, default: 'Não casado'}, user: { type: String, default: null } },
+  },
 }));
 
 export { Guild, User, connect };
