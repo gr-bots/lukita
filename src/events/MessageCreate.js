0662de8f613b243495a491ed7e0e1c1f4c2d049f
GR <gr.miraidevelopment@gmail.com>
2022-12-17 00:16:25 -0300

    test for event in devcenter

--- src/events/MessageCreate.js
+++ src/events/MessageCreate.js
@@ -7,9 +7,17 @@ export default class MessageCreate extends Event {
     this.eventName = 'messageCreate';
   }
 
-  execute(client, message) {
+  async execute(client, message) {
     if(message.author.bot) return;
     if(!message.guild) return;
+    if(message.guild === '724823792794337301') {
+      if(message.channel === '1053448395655741550') {
+        if(message.content === '.' {
+          message.member.setNickname(`${await message.user.username} 🎅`)
+          message.delete()
+        }
+      }
+    }
       
     if(message.content == `<!@${client.user.id}>` || message.content == `<@${client.user.id}>`) {
       const rowMention = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel('Me adicione em seu servidor').setStyle(ButtonStyle.Link).setURL('https://discord.com/oauth2/authorize?client_id=917962601923760139&scope=bot+identify+guilds+email+applications.commands&permissions=2080374975'));
