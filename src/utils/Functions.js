9e5c186e8fff369bda83dd22b349b6676b98741a
guihrib <gr.miraidevelopment@gmail.com>
2022-12-18 19:22:48 -0300

    test

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -36,20 +36,7 @@ class Tools {
     }),
     
     this.toHex = (function(d) {
-      if (s.substr(0,2).toLowerCase() == "0x") {
-        return s;
-      }
-      var l = "0123456789ABCDEF";
-      var o = "";
-  
-      if (typeof s != "string") {
-          s = s.toString();
-      }
-      for (var i=0; i<s.length; i++) {
-        var c = s.charCodeAt(i);
-        o = o + l.substr((c>>4),1) + l.substr((c & 0x0f),1);
-      }
-      return "0x" + o;
+      return d.toString(16);
   })
 
     this.randomHex = (function () {
