a5c5073fe6b31ed73a824bed9894035ca77f059c
guidsribeiro <its.gr6603@gmail.com>
2023-04-16 13:42:00 -0300

    new deploy

--- src/events/InteractionCreate.js
+++ src/events/InteractionCreate.js
@@ -7,7 +7,6 @@ export default class InteractionCreate extends Event {
 
     async execute(client, interaction) {
         if (!interaction.isChatInputCommand()) return;
-        if (!interaction.isStringSelectMenu()) return;
         if (!interaction.guild) return;
 
         const command = client.commands.getCommand(interaction.commandName);
@@ -18,10 +17,10 @@ export default class InteractionCreate extends Event {
         try {
             if (command.options.devOnly == true && !client.dev.some((id) => id === interaction.user.id)) return interaction.reply({ content: `⚠️・<@${interaction.user.id}>, Você não é meu desenvolvedor.`, fetchReply: true, ephemeral: true });
 
-            await command?.execute({ client, interaction })
+            await command?.execute({ client, interaction });
         } catch (error) {
             console.log(error.stack)
-            await interaction.reply({ content: `> ⚠️・<@${interaction.user.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true, fetchReply: true })
+            await interaction.reply({ content: `> ⚠️・<@${interaction.user.id}>, Ocorreu um erro ao executar o comando! Já avisei meu desenvolvedor.`, ephemeral: true, fetchReply: true });
         }
     }
 }
