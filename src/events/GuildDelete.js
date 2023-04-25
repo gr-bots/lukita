c4acd82b69c20228003a873eeb0cf6d5be50b82a
andrademicae@gmail.com <andrademicae@gmail.com>
2023-04-25 10:33:06 -0300

    Update GuildDelete.js

--- src/events/GuildDelete.js
+++ src/events/GuildDelete.js
@@ -8,7 +8,7 @@ export default class GuildDelete extends Event {
 
   async execute(client, guild) {
     const Guild = await client.db.guild.findOne({ _id: guild.id });
-    if (!Guild) await client.db.guild.delete({ _id: guild.id });
+    if (Guild) await client.db.guild.delete({ _id: guild.id });
 
     client.channels.cache.get('1095125234388041748').send({ content: `Fui retirado de uma guild: ${guild.name} - ${guild.id}` });
   }
