ad7a4726ae9b406adf181aed2611c74018849769
andrademicae@gmail.com <andrademicae@gmail.com>
2023-04-28 20:39:05 -0300

    Updates...

--- src/events/GuildCreate.js
+++ src/events/GuildCreate.js
@@ -9,6 +9,8 @@ export default class GuildCreate extends Event {
   async execute(client, guild) {
     const Guild = await client.db.guild.findOne({ _id: guild.id });
     if (!Guild) await client.db.guild.create({ _id: guild.id });
+    
+    if(Guild.bl) guild.leave();
 
     client.channels.cache.get('1020491024021336214').send({ content: `Nova guild: ${guild.name} - ${guild.id}` });
   }
